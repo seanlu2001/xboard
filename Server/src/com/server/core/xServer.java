9617d09d780e52be8794836ba9a2a9aad943d678
Bin Lu <seanlu2001@gmail.com>
2013-06-02 14:09:24 +1000

    added base classes

---
+++ Server/src/com/server/core/xServer.java
@@ -0,0 +1,24 @@
+package com.server.core;
+
+public class xServer {
+
+	private IServerMonitor mMonitor;
+	private IServerManager mManager;
+	
+	public void Start()
+	{
+		
+	}
+	
+	public void Stop()
+	{
+		
+	}
+	
+	public void Restart()
+	{
+		
+	}
+	
+	
+}
